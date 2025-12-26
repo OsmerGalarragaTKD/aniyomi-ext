5dda09027b73e98e1b894a74a53ab5eb1c051aab
OsmerGalarragaTKD <osmergalarraga@gmail.com>
2025-12-26 01:38:06 -0400

    qqq

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
-
+ 
 allprojects {
     repositories {
         mavenCentral()
